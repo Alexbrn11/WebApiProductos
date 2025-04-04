c1304cbaf41fb97271fb56f2b7d52243c4cafeab
AlexanderTituaña <bryan.crew11@gmail.com>
2025-04-03 22:42:46 -0500

    Agregar archivos de proyecto.

---
+++ WebApiProductos/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiProductos.Models;
+
+namespace WebApiProductos.Context
+{
+    public class AppDbContext : DbContext
+    {
+            public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+            {
+
+            }
+            public DbSet<Product> Products { get; set; }
+        }
+}
---
+++ WebApiProductos/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiProductos.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public required string Description { get; set; }
+        public required int Precio { get; set; }
+    }
+}
